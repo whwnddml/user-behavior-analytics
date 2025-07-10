28de31706e23d153066028d2c4903973c97891df
whwnddml <junny4u@gmail.com>
2025-07-10 11:35:49 +0900

    에러페이지 수정

--- backend/src/middlewares/error.ts
+++ backend/src/middlewares/error.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import logger from '../config/logger';
+import { logger } from '../config/logger';
 
 export const errorHandler = (
   err: Error,
